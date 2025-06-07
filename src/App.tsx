dddf186fea7e50258777cdcee13cd6fc8a8eaeeb
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-06-07 17:41:51 +0900

    Design: 로그인 회원가입 페이지 구현
    로그인, 회원가입 요청 기능도 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,35 @@ import DashBoard from "./pages/dash_board/DashBoard";
 import Layout from "./components/Layout";
 import InsertConferenceInfo from "./pages/inerst_conference_info/InsertConferenceInfo";
 import Result from "./pages/result/Result";
+import SignUp from "./pages/sign_up/SignUp";
+import SignIn from "./sign_in/SignIn";
+import { useEffect } from "react";
+import SocialSignUp from "./pages/social_sign_up/SocialSignUp";
 
 function App() {
+  async function checkAuth() {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/v1/users/auth/check`,
+      {
+        method: "GET",
+        credentials: "include", // 쿠키 포함 필수
+      }
+    );
+
+    if (res.ok) {
+      const data = await res.json();
+      console.log("현재 접속중:", data);
+      return data.authenticated;
+    } else {
+      console.log("로그인 필요");
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <Routes>
       <Route element={<Layout />}>
@@ -13,6 +40,9 @@ function App() {
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/insert_info" element={<InsertConferenceInfo />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/social_sign_up" element={<SocialSignUp />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/social_sign_up/SocialSignUp.tsx
@@ -0,0 +1,237 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import SignUpSuccessModal from "../sign_up/SignUpSuccessModal";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 1rem;
+  background: linear-gradient(to bottom, #bca0d6, #3d1454);
+  min-height: 100vh;
+`;
+
+export const FormContainer = styled.div`
+  background-color: white;
+  padding: 3rem 2rem;
+  border-radius: 2rem;
+  width: 100%;
+  max-width: 500px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #4a1168;
+  margin-bottom: 2rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 0.5rem;
+  font-weight: 600;
+  color: #333;
+`;
+
+export const Input = styled.input`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  border: 1px solid #ccc;
+  font-size: 1rem;
+`;
+
+export const Select = styled.select`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  border: 1px solid #ccc;
+  font-size: 1rem;
+`;
+
+export const SubmitButton = styled.button`
+  margin-top: 2rem;
+  background-color: #4a1168;
+  color: white;
+  font-size: 1.1rem;
+  font-weight: bold;
+  padding: 0.75rem;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3a0d54;
+  }
+`;
+
+const SocialSignUp: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    username: "",
+    // password: "",
+    confirmPassword: "",
+    company: "",
+    department: "",
+    team: "",
+  });
+  const [showModal, setShowModal] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // if (formData.password !== formData.confirmPassword) {
+    //   alert("비밀번호가 일치하지 않습니다.");
+    //   return;
+    // }
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/v1/users/social_signup`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            // name: formData.name,
+            // email: formData.email,
+            login_id: formData.username,
+            password: "none",
+            phone: formData.phone,
+            company: formData.company,
+            department: formData.department,
+            team: formData.team,
+            position: "aaf44bda-6a64-4611-b0ca-4083b59c8e6e",
+            job: "개발자",
+            sysrole: "c4cb5e53-617e-463f-8ddb-67252f9a9742",
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "회원가입 실패");
+      }
+
+      setShowModal(true); // 회원가입 성공 시 모달 열기
+    } catch (error: any) {
+      console.error("error:", error);
+      alert(`오류 발생: ${error.message}`);
+    }
+  };
+  return (
+    <>
+      <Wrapper>
+        <FormContainer>
+          <Title>소셜 회원가입</Title>
+          <Form onSubmit={handleSubmit}>
+            {/* <InputGroup>
+              <Label>이름 *</Label>
+              <Input
+                name="name"
+                type="text"
+                placeholder="이름을 입력하세요"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup> */}
+            {/* <InputGroup>
+              <Label>이메일주소 *</Label>
+              <Input
+                name="email"
+                type="email"
+                placeholder="이메일을 입력하세요"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup> */}
+            <InputGroup>
+              <Label>핸드폰번호 *</Label>
+              <Input
+                name="phone"
+                type="tel"
+                placeholder="- 없이 숫자만 입력"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup>
+            <InputGroup>
+              <Label>아이디 *</Label>
+              <Input
+                name="username"
+                type="text"
+                placeholder="영문소문자/숫자, 6~16자"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup>
+            {/* <InputGroup>
+              <Label>비밀번호 *</Label>
+              <Input
+                name="password"
+                type="password"
+                placeholder="영문+숫자+특수문자 조합, 8~16자"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup>
+            <InputGroup>
+              <Label>비밀번호 확인 *</Label>
+              <Input
+                name="confirmPassword"
+                type="password"
+                placeholder="비밀번호를 다시 입력하세요"
+                required
+                onChange={handleChange}
+              />
+            </InputGroup> */}
+            <InputGroup>
+              <Label>소속 회사명 *</Label>
+              <Select name="company" required onChange={handleChange}>
+                <option value="">선택</option>
+                <option value="3db3ef9a-947e-4237-93da-d306b7bdb52d">
+                  회사A
+                </option>
+                {/* <option value="회사B">회사B</option> */}
+              </Select>
+            </InputGroup>
+            <InputGroup>
+              <Label>소속 부서명</Label>
+              <Input name="department" type="text" onChange={handleChange} />
+            </InputGroup>
+            <InputGroup>
+              <Label>소속 팀명</Label>
+              <Input name="team" type="text" onChange={handleChange} />
+            </InputGroup>
+            <SubmitButton type="submit">가입 완료</SubmitButton>
+          </Form>
+        </FormContainer>
+      </Wrapper>
+      <SignUpSuccessModal
+        visible={showModal}
+        onClose={() => setShowModal(false)}
+      />
+    </>
+  );
+};
+
+export default SocialSignUp;
