46f0a0f61d3ccf4a05bbfaa2f73607e1c2ee2ada
kkameoo <khdg1202@naver.com>
2025-06-09 14:04:12 +0900

    Chore: 안쓰는 변수 삭제

--- src/sign_in/SignIn.tsx
+++ src/sign_in/SignIn.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import type { ChangeEvent, FormEvent } from 'react';
+import type { ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
