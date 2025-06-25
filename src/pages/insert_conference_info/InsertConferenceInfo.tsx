4ba69bd68748b166937d90bd949343877f93879f
ehguss <dohyeongim29@gmail.com>
2025-06-25 18:28:30 +0900

    Feat: 참석자 정보를 이름과 이메일이 아닌 user_id로 비교해서 select하도록 수정

--- src/pages/insert_conference_info/InsertConferenceInfo.tsx
+++ src/pages/insert_conference_info/InsertConferenceInfo.tsx
@@ -193,6 +193,7 @@ const InsertConferenceInfo: React.FC = () => {
       const filteredAttendees = attendees.filter(
         (a) => a.user_id && a.user_id !== hostId
       );
+      const attendeesUserId = filteredAttendees.map((a) => a.user_id);
       const attendeesName = filteredAttendees.map((a) => a.name);
       const attendeesEmail = filteredAttendees.map((a) => a.email);
       const attendeesRole = filteredAttendees.map((a) => a.user_jobname);
@@ -207,9 +208,11 @@ const InsertConferenceInfo: React.FC = () => {
         'meeting_date',
         meetingDate ? formatDateToKST(meetingDate) : ''
       );
+      formData.append('host_id', hostId);
       formData.append('host_name', hostName);
       formData.append('host_email', hostEmail);
       formData.append('host_role', hostJobname);
+      attendeesUserId.forEach((id) => formData.append('attendees_ids', id));
       attendeesName.forEach((name) => formData.append('attendees_name', name));
       attendeesEmail.forEach((email) => formData.append('attendees_email', email));
       attendeesRole.forEach((role) => formData.append('attendees_role', role));
