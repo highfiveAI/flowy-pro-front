7c516aa5c5a9621b19b7843515612965faa10d20
kkameoo <khdg1202@naver.com>
2025-06-30 12:24:42 +0900

    Fix: npm run build후 수정

--- src/pages/chatbot/ChatBot.tsx
+++ src/pages/chatbot/ChatBot.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { sendChatMessage } from '../../api/fetchChatbot';
 
 const Container = styled.div`
